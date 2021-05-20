8193a0f496a30c159ddb90cd8e7775f0d9c6b1b4
Anshul Riyal <anshul.riyal@magnontbwa.com>
2021-05-20 18:33:27 +0530

    Adding routes and Migration for article,user & merge_file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :articles
+      resources :users do
+        resources :articles
+      end
+      post 'update_doc' => 'merge_files#merge'
     end
   end
 end
